37b10c7657aac93bca27a314e74022c770b7ec27
zouxinjie <zouxinjie110@126.com>
2019-09-16 23:56:56 +0800

    init

---
+++ src/components/home/hslider.vue
@@ -0,0 +1,36 @@
+<template>
+    <article class="">
+        <slider/>
+        <section class="list">
+            <div class="item" v-for="item in enters" :key="item.img">
+                <router-link :to="{ name: item.href }">
+                    <img :src="item.img" :alt="item.title">
+                    <h4>{{ item.title }}</h4>
+                </router-link>
+            </div>
+        </section>
+    </article>
+</template>
+
+<script>
+    import Slider from '../../core/slider'
+
+    export default {
+        name: 'hslider',
+        components: {Slider},
+        data(){
+            return {
+                items: [],
+                enters: [{
+                    href:'',
+                    img: '',
+                    title: '',
+                }]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/core/slider.vue
@@ -0,0 +1,50 @@
+<template lang="html">
+    <section :class="cname">
+        <swiper :option="options" :not-next-tick="options.notNextTick">
+            <swiper-slide v-for="item in items" :key="item.href">
+                <router-link :to="{name: item.href}">
+                    <img :src="item.src" alt="">
+                </router-link>
+            </swiper-slide>
+            <div class="swiper-pagination" v-if="options.pagination"></div>
+        </swiper>
+    </section>
+</template>
+
+<script>
+import {swiper, swiperSlide} from 'vue-awesome-swiper'
+
+export default {
+  name: 'slider',
+  components: {
+    swiper,
+    swiperSlide
+  },
+  props: {
+    options: {
+      type: Object,
+      default () {
+        return {
+          autoplay: true,
+          loop: true,
+          pagination: {
+            el: '.swiper-pagination'
+          },
+          notNextTick: false // 下一个点击时间
+        }
+      }
+    },
+    items: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+    @import '~swiper/dist/css/swiper.css';
+
+</style>
